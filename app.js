7dc39a794b4fdf44cf5839640e0eeeae9267ac25
PavanYata <pavanyata123@gmail.com>
2024-07-21 19:06:24 +0530

    first commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var usersRouter = require('./app_server/routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'app_server', 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-var dbURI = "";
+var dbURI = "mongodb+srv://22eg107c22:oVSMMko3vf5VWQY7@cluster0.tqarckx.mongodb.net/LOCATOR";
 mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', () => {
-  console.log(`Mongoose connected to ${dbURI}`);
+  console.log('Mongoose connected to ${dbURI}');
 });
 mongoose.connection.on('error', err => {
   console.log('Mongoose connection error:', err);
@@ -12,5 +12,4 @@ mongoose.connection.on('disconnected', () => {
   console.log('Mongoose disconnected');
 });
 
-require('./locations');
-pby-smna-cbd
+require('./locations.js');
