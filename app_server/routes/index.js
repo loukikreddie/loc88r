a24f480651c2719107eee755f555187e6376df1a
loukikreddie2203 <149510392+loukikreddie@users.noreply.github.com>
2024-08-28 19:10:48 +0530

    Update index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'location-list' });
-});
+router.get('/', ctrlMain.index);
 
-router.get('/location-info', function(req, res, next) {
-  res.render('index', { title: 'location-info' });
+router.get('/location', function(req, res, next) {
+  res.render('index', { title: 'Location info' });
 });
-
-router.get('/location-info/review', function(req, res, next) {
-  res.render('index', { title: 'review page' });
+router.get('/location/review', function(req, res, next) {
+  res.render('index', { title: 'Review' });
 });
-
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'about us' });
+  res.render('index', { title: 'about' });
 });
+router.get('/signin',ctrlMain.signin );
 
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'register' });
+});
+//exports command//
 module.exports = router;
-
